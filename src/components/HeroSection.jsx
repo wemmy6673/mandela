56c80b93313d7a5b008654836d85383b4baa540d
timmypelumy <timmypelumy@gmail.com>
2025-06-06 10:52:53 +0100

    feat: images

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -182,12 +182,12 @@ const SocialInfo = () => {
 						<path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
 					</motion.svg>
 					<a
-						href="https://x.com/MNDLdotFUN"
+						href="https://x.com/MNDLzone"
 						target="_blank"
 						rel="noopener noreferrer"
 						className="text-gray-300  hover:text-cyan-400 transition-colors duration-300 font-medium tracking-widest  sm:text-base md:text-lg truncate min-w-0"
 					>
-						@MNDLdotFUN
+						@MNDLzone
 					</a>
 				</div>
 			</motion.div>
